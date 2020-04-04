ac215d3df74a0e9df5da1a4abcd49a0d716ea513
akt-sMain <tuka6626@gmail.com>
2020-04-05 00:32:06 +0900

    パッケージ作成時に確認ダイアログを表示しないようにした

--- mk_package.bat
+++ mk_package.bat
@@ -3,7 +3,7 @@ md JamPlayer
 copy JamPlayer.exe JamPlayer\JamPlayer.exe
 copy project\JMP\JamPlayer.jar JamPlayer\JamPlayer.jar
 copy clean.bat JamPlayer\clean.bat
-copy clean.bat JamPlayer\clean_all.bat
-xcopy /e project\JMP\skin JamPlayer\skin
+copy clean_all.bat JamPlayer\clean_all.bat
+xcopy /y /e skin JamPlayer\skin\
 
 pause
