c37b4dfaea58144d797ebe96937c6950d3609767
akt-sMain <tuka6626@gmail.com>
2020-07-23 23:47:42 +0900

    サンプルプラグイン（JMPConfigビューワー）を追加

---
+++ JMPConfig/src/JMPConfig.java
@@ -0,0 +1,86 @@
+import jlib.plugin.JMidiPlugin;
+
+public class JMPConfig extends JMidiPlugin {
+
+    public static JMPConfigFrame MainFrame = null;
+
+    public static void main(String[] args) {
+        System.out.println("**JMPConfig**");
+    }
+
+    public JMPConfig() {
+    }
+
+    @Override
+    public void close() {
+        /*
+         * プラグインが閉じられた際の処理を記述します。
+         */
+        MainFrame.setVisible(false);
+    }
+
+    @Override
+    public void exit() {
+        /*
+         * JamPlayerが終了された際の処理を記述します。
+         */
+        if (MainFrame != null) {
+            MainFrame.dispose();
+        }
+    }
+
+    @Override
+    public boolean isOpen() {
+        /*
+         * プラグインが開かれている状態を示す記述します。
+         */
+        return MainFrame.isVisible();
+    }
+
+    @Override
+    public void initialize() {
+        /*
+         * JamPlayerが起動された際の処理を記述します。
+         */
+        MainFrame = new JMPConfigFrame();
+    }
+
+    @Override
+    public void open() {
+        /*
+         * プラグインが開かれた際の処理を記述します。
+         */
+        MainFrame.setVisible(true);
+    }
+
+    @Override
+    protected void noteOn(int channel, int midiNumber, int velocity, long timeStamp, short senderType) {
+        /*
+         * ノートオンイベントを送信した際の処理を記述します。
+         */
+        MainFrame.setNoteOn(channel);
+    }
+
+    @Override
+    protected void noteOff(int channel, int midiNumber, long timeStamp, short senderType) {
+        /*
+         * ノートオフイベントを送信した際の処理を記述します。
+         */
+        MainFrame.setNoteOff(channel);
+    }
+
+    @Override
+    protected void programChange(int channel, int programNumber, long timeStamp, short senderType) {
+        /*
+         * プログラムチェンジイベントを送信した際の処理を記述します。
+         */
+    }
+
+    @Override
+    protected void pitchBend(int channel, int pbValue, long timeStamp, short senderType) {
+        /*
+         * ピッチベンドイベントを送信した際の処理を記述します。
+         */
+    }
+
+}
---
+++ JMPConfig/src/JMPConfigFrame.java
@@ -0,0 +1,281 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+
+import jlib.core.JMPCoreAccessor;
+import jlib.gui.IJmpWindow;
+
+public class JMPConfigFrame extends JFrame {
+
+    public static final Color COLOR_NOTE_ON = Color.GREEN;
+    public static final Color COLOR_NOTE_OFF = Color.RED;
+
+    public static final String TYPE_CONFIG = "CFG";
+    public static final String TYPE_SYSTEM = "SYS";
+    public static final String TYPE_WINDOW = "WIN";
+
+    private JPanel contentPane;
+    private JTable table;
+    private JPanel panel;
+    private JButton btnApply;
+    private JButton btnUpdate;
+    private DefaultTableModel model;
+    private JLabel label;
+    private JLabel label_1;
+    private JLabel label_2;
+    private JLabel label_3;
+    private JLabel label_4;
+
+    private JLabel[] chMonitor = null;
+    private JPanel panel_1;
+    private JLabel label_5;
+    private JLabel label_6;
+    private JLabel label_7;
+    private JLabel label_8;
+    private JLabel label_9;
+    private JLabel label_10;
+    private JLabel label_11;
+    private JLabel label_12;
+    private JLabel label_13;
+    private JLabel label_14;
+    private JLabel label_15;
+    private JButton btnPlayStop;
+
+    /**
+     * Create the frame.
+     */
+    public JMPConfigFrame() {
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                setVisible(false);
+            }
+        });
+        setBounds(100, 100, 569, 395);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setLayout(new BorderLayout(0, 0));
+        setContentPane(contentPane);
+
+        JScrollPane scrollPane = new JScrollPane();
+        contentPane.add(scrollPane, BorderLayout.CENTER);
+
+        model = new DefaultTableModel();
+        table = new JTable(model);
+        table.setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {
+            @Override
+            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int col) {
+
+                super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, col);
+
+                String type = model.getValueAt(row, 2).toString();
+                if (isSelected == true) {
+                    // 選択行
+                }
+                else {
+                    if (type.equals(TYPE_CONFIG) == true) {
+                        setBackground(Color.GREEN);
+                    }
+                    else if (type.equals(TYPE_WINDOW) == true) {
+                        setBackground(Color.ORANGE);
+                    }
+                    else {
+                        setBackground(Color.LIGHT_GRAY);
+                    }
+                }
+                return this;
+            }
+        });
+        model.addColumn("Key");
+        model.addColumn("Value");
+        model.addColumn("Type");
+        scrollPane.setViewportView(table);
+
+        panel = new JPanel();
+        contentPane.add(panel, BorderLayout.SOUTH);
+
+        btnUpdate = new JButton("Update");
+        btnUpdate.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                updateTable();
+            }
+        });
+        panel.add(btnUpdate);
+
+        btnApply = new JButton("Apply");
+        btnApply.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                applyTable();
+            }
+        });
+        panel.add(btnApply);
+
+        panel_1 = new JPanel();
+        contentPane.add(panel_1, BorderLayout.NORTH);
+
+        label = new JLabel("■");
+        panel_1.add(label);
+
+        label_1 = new JLabel("■");
+        panel_1.add(label_1);
+
+        label_2 = new JLabel("■");
+        panel_1.add(label_2);
+
+        label_3 = new JLabel("■");
+        panel_1.add(label_3);
+
+        label_4 = new JLabel("■");
+        panel_1.add(label_4);
+
+        label_5 = new JLabel("■");
+        panel_1.add(label_5);
+
+        label_6 = new JLabel("■");
+        panel_1.add(label_6);
+
+        label_7 = new JLabel("■");
+        panel_1.add(label_7);
+
+        label_8 = new JLabel("■");
+        panel_1.add(label_8);
+
+        label_9 = new JLabel("■");
+        panel_1.add(label_9);
+
+        label_10 = new JLabel("■");
+        panel_1.add(label_10);
+
+        label_11 = new JLabel("■");
+        panel_1.add(label_11);
+
+        label_12 = new JLabel("■");
+        panel_1.add(label_12);
+
+        label_13 = new JLabel("■");
+        panel_1.add(label_13);
+
+        label_14 = new JLabel("■");
+        panel_1.add(label_14);
+
+        label_15 = new JLabel("■");
+        panel_1.add(label_15);
+
+        btnPlayStop = new JButton("Play/Stop");
+        btnPlayStop.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                actionPlayStop();
+            }
+        });
+        panel_1.add(btnPlayStop);
+
+        chMonitor = new JLabel[] { label, label_1, label_2, label_3, label_4, label_5, label_6, label_7, label_8, label_9, label_10, label_11, label_12,
+                label_13, label_14, label_15, };
+
+        for (JLabel lb : chMonitor) {
+            lb.setForeground(COLOR_NOTE_OFF);
+        }
+    }
+
+    private void actionPlayStop() {
+        /*
+         * 再生/停止を操作する。
+         */
+        JMPCoreAccessor.getSoundManager().togglePlayStop();
+    }
+
+    private void applyTable() {
+        for (int i = model.getRowCount() - 1; i >= 0; i--) {
+            String key = model.getValueAt(i, 0).toString();
+            String val = model.getValueAt(i, 1).toString();
+            String type = model.getValueAt(i, 2).toString();
+
+            if (type.equals(TYPE_CONFIG) == true) {
+                /*
+                 * Configパラメータを設定する。
+                 */
+                JMPCoreAccessor.getDataManager().setConfigParam(key, val);
+            }
+            else if (type.equals(TYPE_WINDOW) == true) {
+                /*
+                 * Windowのステータス(SHOW/HIDE)を設定する。
+                 */
+                IJmpWindow win = JMPCoreAccessor.getWindowManager().getWindow(key);
+                if (val.equalsIgnoreCase("SHOW") == true) {
+                    if (win.isWindowVisible() == false) {
+                        win.showWindow();
+                    }
+                }
+                else {
+                    if (win.isWindowVisible() == true) {
+                        win.hideWindow();
+                    }
+                }
+            }
+            else if (type.equals(TYPE_SYSTEM) == true) {
+                /*
+                 * System共通レジスタ値を取得する。
+                 */
+                JMPCoreAccessor.getSystemManager().setCommonRegisterValue(key, val);
+            }
+        }
+    }
+
+    private void updateTable() {
+        /* テーブルアイテムを更新 */
+        for (int i = model.getRowCount() - 1; i >= 0; i--) {
+            model.removeRow(i);
+        }
+        /*
+         * Configパラメータを全て取得する。
+         */
+        for (String key : JMPCoreAccessor.getDataManager().getKeySet()) {
+            Object[] rows = { key, JMPCoreAccessor.getDataManager().getConfigParam(key), TYPE_CONFIG };
+            model.addRow(rows);
+        }
+        /*
+         * Windowのステータス(SHOW/HIDE)を取得する。
+         */
+        for (String key : JMPCoreAccessor.getWindowManager().getWindowNameList()) {
+            IJmpWindow win = JMPCoreAccessor.getWindowManager().getWindow(key);
+            if (win != null) {
+                Object[] rows = { key, win.isWindowVisible() ? "SHOW" : "HIDE", TYPE_WINDOW };
+                model.addRow(rows);
+            }
+        }
+        /*
+         * System共通レジスタ値を取得する。
+         */
+        for (String key : JMPCoreAccessor.getSystemManager().getCommonKeySet()) {
+            Object[] rows = { key, JMPCoreAccessor.getSystemManager().getCommonRegisterValue(key), TYPE_SYSTEM };
+            model.addRow(rows);
+        }
+    }
+
+    public void setNoteOn(int channel) {
+        if (0 <= channel && channel < chMonitor.length) {
+            chMonitor[channel].setForeground(COLOR_NOTE_ON);
+        }
+    }
+
+    public void setNoteOff(int channel) {
+        if (0 <= channel && channel < chMonitor.length) {
+            chMonitor[channel].setForeground(COLOR_NOTE_OFF);
+        }
+    }
+
+}
